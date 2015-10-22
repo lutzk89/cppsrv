74c4c576679108eedb904df6609051e4c5f3b700
lutz <lutz.kuenneke89@gmx.de>
2015-10-22 22:35:03 +0200

    ads

---
+++ service/dataprovider.cpp
@@ -0,0 +1,15 @@
+#include<dbhandler.h>
+class dirDataProvider: public dataProvider {
+	private:
+	dirDbhandler* handler;
+	char* dirName;
+	public:
+	dirDataProvider(char* dirName) {
+		this->dirName = dirName;
+		this->handler = new dirDbhandler<double>(this->dirname);
+		this->handler->read_dir();
+	}
+	int read() {
+		return 0;
+	}
+};
